7cac4f1dd767dbb8a5a386bbb276a05cb1fb8ed2
Emilio <galindo.hayashi@gmail.com>
2020-06-20 14:55:31 -0500

    welcome msg

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+  before_action :set_user, only: [:index]
+
   def index
+    flash[:notice] = "Bienvenido #{@user.email}."
+  end
+
+  private 
+  def set_user
+    @user = User.find(current_user.id)
   end
 end

b58f82d38bac1119122c5ded49245f93a8aee3bf
Emilio <galindo9810@hotmail.com>
2020-06-22 17:39:57 -0500

    add default role to users

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2020_06_22_003613) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "firstname"
     t.string "lastname"
-    t.integer "role_id", null: false
+    t.integer "role_id", default: 1, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["role_id"], name: "index_users_on_role_id"
